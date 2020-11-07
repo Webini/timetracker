d24b34f5bc6951295174881bead5c453e95de667
Nicolas GASPARINI <nico@webini.fr>
2020-11-07 11:23:02 +0100

    fix missing field in refreshToken

--- src/EventListener/AuthenticationSuccessListener.php
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -6,21 +6,44 @@ namespace App\EventListener;
 
 use App\Entity\User;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class AuthenticationSuccessListener
 {
+    /**
+     * @var UserProviderInterface
+     */
+    private $userProvider;
+
+    /**
+     * AuthenticationSuccessListener constructor.
+     * @param UserProviderInterface $userProvider
+     */
+    public function __construct(UserProviderInterface $userProvider)
+    {
+        $this->userProvider = $userProvider;
+    }
+
     public function onJWTCreated(JWTCreatedEvent $event)
     {
         $data = $event->getData();
         $user = $event->getUser();
+        $userEntity = null;
 
-        if (!$user instanceof User) {
-            return;
+        if ($user instanceof User) {
+            $userEntity = $user;
+        } else if ($user instanceof \Symfony\Component\Security\Core\User\User) {
+            $userEntity = $this->userProvider->loadUserByUsername($user->getUsername());
+        }
+
+        if ($userEntity === null) {
+            throw new \Exception('Invalid user');
         }
 
         $event->setData(array_merge($data, [
-            'roles' => $user->getRoles(),
-            'id' => $user->getId(),
+            'roles' => $userEntity->getRoles(),
+            'id' => $userEntity->getId(),
         ]));
     }
 
